601c1f004bdbb2d67f7865e44299f7ab064171b5
Phil Oakley <phil.oakley@gmail.com>
2012-09-10 22:18:48 -0400

    Testing slowdown
    Code plots fake data using the simple plot technique.

--- read_plot_pm.py
+++ read_plot_pm.py
@@ -29,6 +29,7 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
+from matplotlib.lines import Line2D
 
 
 import serial
@@ -79,7 +80,11 @@ class Position_data():
 		self.byte_to_int = []
 		self.bit15array = []
 		self.valuearray = []
-
+		
+		self.x1_position = []
+		self.y1_position = []
+		self.position = []
+		
 
 	def connect_to_usb(self):
 		'''Connect to USB feed'''
@@ -90,11 +95,13 @@ class Position_data():
 		#Replace above with command line or GUI option menu
 		
 		self.port = '/dev/tty.usbserial-FTT3QDXK'
+		#self.port = '/dev/tty.usbmodem411'
 		self.baudrate = 115200
 		self.parity = 'N'
 		self.rtscts = False
 		self.xonxoff = False
-		self.ser = serial.Serial(self.port, self.baudrate, parity=self.parity, rtscts=self.rtscts, xonxoff=self.xonxoff, timeout=1)
+		#self.ser = serial.Serial(self.port, self.baudrate, parity=self.parity, rtscts=self.rtscts, xonxoff=self.xonxoff, timeout=1)
+		self.ser = serial.Serial(self.port, self.baudrate)
 		
 		print 'Setting up USB connection:'
 		print 'Port: ' + self.port
@@ -108,16 +115,16 @@ class Position_data():
 	def calculate_positions(self):
 		'''Calculate laser spot position from diode values'''
 		#Calculate spot positions
-		self.x1_position = self.valuearray[1] / self.valuearray[0]
-		self.y1_position = self.valuearray[3] / self.valuearray[2]
+		self.x1_position.extend(self.valuearray[1] / self.valuearray[0])
+		self.y1_position.extend(self.valuearray[3] / self.valuearray[2])
 		self.x2_position = self.valuearray[5] / self.valuearray[4]
 		self.y2_position = self.valuearray[7] / self.valuearray[6]
 		self.x3_position = self.valuearray[9] / self.valuearray[8]
 		self.y3_position = self.valuearray[11] / self.valuearray[10]
 		self.x4_position = self.valuearray[13] / self.valuearray[12]
 		self.y4_position = self.valuearray[15] / self.valuearray[14]
-	
-		self.position = [self.x1_position,self.y1_position,self.x2_position,self.y2_position,self.x3_position,self.y3_position,self.x4_position,self.y4_position]
+			
+		self.position = [self.x1_position,self.y1_position]#,self.x2_position,self.y2_position,self.x3_position,self.y3_position,self.x4_position,self.y4_position]
 	
 	def convert_from_twos_complement(self,value):
 		'''Convert from twos complement'''
@@ -143,9 +150,10 @@ class Position_data():
 		
 			#Read in a byte (will be an ascii character)
 			sync_test_byte = self.ser.read(1)
-			
+
 			#Convert byte to integer (needed to perform operations on)
 			sync_test_int = ord(sync_test_byte)
+			print sync_test_int
 			
 			#Check to see if the first bit is a 1
 			#This happens when the overall integer value is >= 128
@@ -157,11 +165,13 @@ class Position_data():
 			sync_attempt += 1
 			
 		print 'Failed to sync in: ' + str(sync_attempt) + ' attempts'
+		#sys.exit()
 		return 0;
 
 	
 	def read_cycle(self):
 		'''Reads a full 32 byte cycle'''
+		print time.time()
 		sync_attempt=0
 		
 		self.cycle_byte = []
@@ -170,7 +180,7 @@ class Position_data():
 		if (self.fake_data == False):
 			self.cycle_byte.append(self.sync_feed(sync_attempt))
 			self.cycle_byte_to_int.append(ord(self.cycle_byte[0]))
-			
+
 			if self.cycle_byte_to_int[0] < 128:
 				print "Failed to Sync. Exiting"
 				#return 0;
@@ -185,12 +195,15 @@ class Position_data():
 		
 			self.calculate_positions()
 		
-			self.print_positions()
+			#self.print_positions()
 		else:
 			r = random.uniform(-3,3)
+			self.x1_position.append(r)
+			self.y1_position.append(r)
 			self.position = np.ones(8)*r
-	
+		
 		yield self.position
+		#yield [self.x1_position, self.y1_position]
 	
 	def read_31_bytes(self):
 		'''Read the next 31 bytes'''
@@ -203,6 +216,7 @@ class Position_data():
 			
 			# return an integer representing the Unicode code point of the character
 			self.cycle_byte_to_int.append(ord(self.cycle_byte[k]))
+			print ord(self.cycle_byte[k])
 		
 			if (k % 2 == 1):
 			
@@ -312,49 +326,52 @@ class Position_plots():
 	def setup_diodes(self):
 		'''Plot the initial diode maps'''
 		self.fig1 = plt.figure()
+		self.line1 = Line2D([],[],color='r*')
 		
 		self.diode1 = self.fig1.add_subplot(2,2,1)
-		self.diode1.plot([],[],'k.')
+		#self.diode1.plot([],[],'k.')
 		self.diode1.set_xlabel('X Position [mm]')
 		self.diode1.set_ylabel('X Position [mm]')
 		self.diode1.set_title('Diode 1')
 		
-		self.diode2 = self.fig1.add_subplot(2,2,2)
-		self.diode2.plot([],[],'k.')
-		self.diode2.set_xlabel('X Position [mm]')
-		self.diode2.set_ylabel('X Position [mm]')
-		self.diode2.set_title('Diode 2')
-		
-		self.diode3 = self.fig1.add_subplot(2,2,3)
-		self.diode3.plot([],[],'k.')
-		self.diode3.set_xlabel('X Position [mm]')
-		self.diode3.set_ylabel('X Position [mm]')
-		self.diode3.set_title('Diode 3')		
-		
-		self.diode4 = self.fig1.add_subplot(2,2,4)
-		self.diode4.plot([],[],'k.')
-		self.diode4.set_xlabel('X Position [mm]')
-		self.diode4.set_ylabel('X Position [mm]')
-		self.diode4.set_title('Diode 4')
-				
+		# self.diode2 = self.fig1.add_subplot(2,2,2)
+# 		self.diode2.plot([],[],'k.')
+# 		self.diode2.set_xlabel('X Position [mm]')
+# 		self.diode2.set_ylabel('X Position [mm]')
+# 		self.diode2.set_title('Diode 2')
+# 		
+# 		self.diode3 = self.fig1.add_subplot(2,2,3)
+# 		self.diode3.plot([],[],'k.')
+# 		self.diode3.set_xlabel('X Position [mm]')
+# 		self.diode3.set_ylabel('X Position [mm]')
+# 		self.diode3.set_title('Diode 3')		
+# 		
+# 		self.diode4 = self.fig1.add_subplot(2,2,4)
+# 		self.diode4.plot([],[],'k.')
+# 		self.diode4.set_xlabel('X Position [mm]')
+# 		self.diode4.set_ylabel('X Position [mm]')
+# 		self.diode4.set_title('Diode 4')
+# 				
 		datasize=0		
 		if (datasize==0):
 			self.diode1.set_xlim([-5,5])
 			self.diode1.set_ylim([-5,5])
-			self.diode2.set_xlim([-5,5])
-			self.diode2.set_ylim([-5,5])
-			self.diode3.set_xlim([-5,5])
-			self.diode3.set_ylim([-5,5])
-			self.diode4.set_xlim([-5,5])
-			self.diode4.set_ylim([-5,5])		
+			# self.diode2.set_xlim([-5,5])
+# 			self.diode2.set_ylim([-5,5])
+# 			self.diode3.set_xlim([-5,5])
+# 			self.diode3.set_ylim([-5,5])
+# 			self.diode4.set_xlim([-5,5])
+# 			self.diode4.set_ylim([-5,5])		
 
 		#self.fig1.show()
-		
+		self.diode1.figure.canvas.draw()
 	
-	def update_display(self, newdata):
+	def update_display(self,newdata):
 		'''Update the plot windows'''
 		#self.update_grid(data)
+		#print newdata
 		self.update_diodes(newdata)
+		#return a
 		
 	def update_grid(self, data):
 		'''Update the pixel grid'''
@@ -362,12 +379,18 @@ class Position_plots():
 	
 	def update_diodes(self, newdata):
 		'''Update the diode maps'''
+		#x1 = newdata[0][:]
+		#y1 = newdata[1][:]
+		#print x1
+		#print y1
+		#self.line1.set_data(x1,y1)
 		self.diode1.plot(newdata[0],newdata[1],'k.')
-		self.diode2.plot(newdata[2],newdata[3],'k.')
-		self.diode3.plot(newdata[4],newdata[5],'k.')
-		self.diode4.plot(newdata[6],newdata[7],'k.')
+		#self.diode2.plot(newdata[2],newdata[3],'k.')
+		#self.diode3.plot(newdata[4],newdata[5],'k.')
+		#self.diode4.plot(newdata[6],newdata[7],'k.')
+		#self.diode1.figure.canvas.draw()
 
-
+		#return self.line1
 
 	
 
@@ -375,7 +398,7 @@ if __name__ == "__main__":
 	#==========================================Setup Parameters
 	
 	#Create class instances
-	data    = Position_data(fake_data=False)
+	data    = Position_data(fake_data=True)
 	display = Position_plots()
 	
 	sync_attempt = 0
@@ -384,7 +407,9 @@ if __name__ == "__main__":
 	#Using the Position Plots Class fig1, get data from the Position Data class (read_cycle function)
 	#and display it using the Position Plots class (update_display function)
 	#Do this every 2 milliseconds
-	ani = animation.FuncAnimation(display.fig1, display.update_display, data.read_cycle, interval=2, blit=False)
+	ani = animation.FuncAnimation(display.fig1, display.update_display, data.read_cycle, interval=1, blit=False)
+
+	
 
 	#Animation doesn't happen without this
 	plt.show()
